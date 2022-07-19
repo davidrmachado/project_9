4c3f6fccc1387752cfac9f1c5b1c881dc263986f
David-Neto <davidrmachado@gmail.com>
2022-07-19 19:25:56 -0300

    Entrega do requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../utils/renderWithRouter';
+
+import NotFound from '../pages/NotFound';
+
+describe('Testes do componente NotFound', () => {
+  it('01. Teste se a página contém o texto "Page requested not found 😭"', () => {
+    renderWithRouter(<NotFound />);
+
+    const textChecked = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
+    });
+
+    expect(textChecked).toBeInTheDocument();
+  });
+
+  it('02. Teste se a página mostra a imagem do Pikachu"', () => {
+    renderWithRouter(<NotFound />);
+
+    const pikachuImg = screen.getByAltText(
+      'Pikachu crying because the page requested was not found',
+    );
+
+    expect(pikachuImg).toHaveAttribute(
+      'src',
+      'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif',
+    );
+  });
+});
